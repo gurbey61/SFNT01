c6ecc525181db9d78f6b0564bcd674d3991e420d
gurbey61 <gurbey61@gmail.com>
2022-06-01 19:53:29 -0500

    Second Commit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
     //Hello Dear Developers
     public static void main(String[] args) {
         System.out.print("Hello GitHub");
+        System.out.print(Math.abs(-61));
     }
 
 }
