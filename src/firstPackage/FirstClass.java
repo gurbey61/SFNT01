78adad6dac9fa22a3bf86f065eae0e9920f8740f
gurbey61 <gurbey61@gmail.com>
2022-06-01 20:25:59 -0500

    Third Commit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
     //Hello Dear Developers
     public static void main(String[] args) {
         System.out.print("Hello GitHub");
-        System.out.print(Math.abs(-61));
+
+        //We will do git collaboration using GitHub
+        //Be ready for them!
     }
 
 }
